38ec94aad363ea889d95dd915333475c58579976
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-22 21:15:32 -0400

    add more logic to TCPConnection

--- src/main/java/server/TCPConnection.java
+++ src/main/java/server/TCPConnection.java
@@ -11,23 +11,40 @@ public class TCPConnection extends Thread {
     DataInputStream input;
     DataOutputStream output;
     Socket socket;
+    private String response;
+    public static final String BAD_DATA = "INVALID_DATA";
 
     public TCPConnection(Socket socket) {
         try {
             this.socket = socket;
             input = new DataInputStream(this.socket.getInputStream());
             output = new DataOutputStream(this.socket.getOutputStream());
-            this.start();
+            //this.start();
         } catch (IOException e) {
             System.out.println("Listening: " + e.getMessage());
         }
     }
 
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getData() {
+        try {
+            return input.readUTF();
+        }
+        catch (IOException e) {
+            System.out.println("TCPConnection: Can't get the client data. Reason: " +  e.getMessage());
+        }
+
+        return BAD_DATA;
+    }
+
     @Override
     public void run() {
         try {
-            String data = input.readUTF();
-            output.writeUTF(data);
+            //String data = input.readUTF();
+            output.writeUTF(response);
             socket.close();
         }
         //TODO: Change these generic messages.
