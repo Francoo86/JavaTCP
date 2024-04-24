be5dbe0807465869f279b740f35de73a3fdf37fc
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-24 07:45:57 -0400

    add uploads directory check and close stream

--- src/main/java/server/services/FileReceiverService.java
+++ src/main/java/server/services/FileReceiverService.java
@@ -4,19 +4,35 @@ import java.io.*;
 
 public class FileReceiverService {
     private static final int BUFFER_SIZE = 8196;
+    private static final String UPLOADS = "uploads/";
+
+    public void createDirectoryIfNotExists() {
+        File dir = new File(UPLOADS);
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+    }
 
     public boolean receiveFile(DataInputStream input, String fileName) {
         try {
+            createDirectoryIfNotExists();
+
             int bytes;
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(UPLOADS + fileName);
 
             long size = input.readLong();
+
+            System.out.println("Leyendo archivo: " + fileName);
             byte[] buffer = new byte[BUFFER_SIZE];
 
             while (size > 0 && (bytes = input.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
                 fileOutputStream.write(buffer,0, bytes);
                 size -= bytes;
             }
+
+            System.out.println("Finalizando lectura de " + fileName);
+
+            fileOutputStream.close();
 
             return true;
         } catch (IOException e) {
