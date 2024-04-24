8640b4602fd705ba33c9e104e82697708c1d7b5f
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-23 21:18:46 -0400

    finish not working GUI for while true

--- src/main/java/genericmenu/JFileChooserPDF.java
+++ src/main/java/genericmenu/JFileChooserPDF.java
@@ -1,38 +1,87 @@
 package genericmenu;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
+import javax.swing.text.JTextComponent;
 
-public class JFileChooserPDF {
-    private static final String TYPES_DESC = "Archivos PDF";
-    private static final String REQUIRED_TYPE = "pdf";
+public class JFileChooserPDF extends JFrame implements ActionListener {
+    //data of the descriptors.
+    private static final String FILES_DESC = "Archivos PDF";
+    private static final String FILETYPE = "pdf";
+
+    //height and width
+    private final int WIDTH = 400;
+    private final int HEIGHT = 150;
+
+    private JTextField txt;
+    private JButton btn;
+    private JLabel helpText;
+    private JButton acceptBtn;
 
     private File selectedFile;
 
-    public boolean show() {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    public JFileChooserPDF() {
+        super("Selección de archivo");
+        setLayout(new FlowLayout());
 
-        FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter(TYPES_DESC, REQUIRED_TYPE);
+        helpText = new JLabel("Seleccione un documento PDF:");
+        add(helpText);
+
+        txt = new JTextField(30);
+        //why the user should edit this?
+        txt.setEditable(false);
+        add(txt);
+
+        btn = new JButton("Buscar.");
+        btn.addActionListener(this);
+        add(btn);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter(FILES_DESC, FILETYPE);
         fileChooser.setFileFilter(pdfFilter);
 
-        int result = fileChooser.showOpenDialog(null);
+        int result = fileChooser.showOpenDialog(this);
 
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
-            return true;
+            //return true;
         } else {
             selectedFile = null;
-            return false;
+            //return false;
         }
     }
 
-    public File getSelectedFile() {
+    public File getFile() {
         return selectedFile;
     }
 
-    public boolean isValid() {
+    public boolean isValidFile() {
         return selectedFile != null;
+    }
+
+    public void tryToShow() {
+        try {
+            SwingUtilities.invokeAndWait(() -> showWindow());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void showWindow(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setVisible(true);
     }
 }
