5a2b9224bebd993e20cab9eabcfa7f4b1aa18a43
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-24 01:07:17 -0400

    add sendFile to TCPClient

--- src/main/java/cliente/TCPClient.java
+++ src/main/java/cliente/TCPClient.java
@@ -6,6 +6,7 @@ public class TCPClient {
     //TODO: Make the host and port usable by env.
     private static final int BASE_PORT = 7896;
     private static final String HOSTNAME = "localhost";
+    private static final int BUFFER_SIZE = 4096;
 
     //Client data.
     private Socket socket;
@@ -44,6 +45,32 @@ public class TCPClient {
         }
 
         return "";
+    }
+
+    public void sendFile(File file) {
+        try{
+            int bytes;
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            output.writeLong(file.length());
+            // break file into chunks
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            while ((bytes=fileInputStream.read(buffer))!=-1){
+                output.write(buffer,0,bytes);
+                output.flush();
+            }
+
+            fileInputStream.close();
+        }
+        catch (IOException e) {
+            System.out.println("TCPClient: Error with " + file.getName() + ". Reason => " + e.getMessage());
+        }
+    }
+
+    public void sendFile(String fileName) {
+        File file = new File(fileName);
+        sendFile(file);
     }
 
     public void closeConn() {
